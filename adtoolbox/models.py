64ffe26609ae9521203e6dc9a90aa2544adcc2f0
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2024-07-25 23:12:11 -0600

    LSTM start

---
+++ adtoolbox/models.py
@@ -0,0 +1,17 @@
+import adm
+import configs
+import torch
+from typing import Iterable, List, Tuple, Dict, Any
+import torch.nn as nn
+
+
+
+torch.manual_seed(1)
+
+class AD_LSTM:
+    def __init__(
+        initial_conditions:dict,
+        t_eval:tuple,
+        species:Iterable,
+    ):
+        pass
