0e524da07a1e57547cc9a7bbcbfcf7fe89fcc631
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2024-07-30 15:41:45 -0600

    a basic dense model (in progress)

--- adtoolbox/models.py
+++ adtoolbox/models.py
@@ -8,7 +8,37 @@ import torch.nn as nn
 class AD_LSTM:
     def __init__(
         initial_conditions:dict,
-        t_eval:tuple,
+        time_res:float,
         species:Iterable,
+        reactions:Iterable,
+
     ):
         pass
+
+AD_Dense:
+    
+    def __init__(
+        initial_conditions:dict,
+        time_res:float,
+        duration:float,
+        species:Iterable,
+        reactions:Iterable,
+        ):
+        pass
+    
+
+    def step(self)->torch.FloatTensor:
+        next_state=self.predict_next()
+        self.state=next_state.copy().detach()
+        return next_state   
+
+    def simulate(self)->torch.FloatTensor:
+        self.reset()
+        simulation=[self.state]
+        t=0
+        while t<self.duration:
+            simulation.append(self.step())
+            t+=self.time_res
+
+        return torch.FloatTensor(simulation)
+
